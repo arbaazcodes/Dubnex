a757e1d2dac0aa53e3a3ba151629e94c660d3b72
Arbaaz <arbaazsince2002@gmail.com>
2026-07-30 16:57:35 +0530

    Add backend  second attempt

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,13 @@ from services.translator_service import translate_text
 from services.whisper_service import detect_language, transcribe_audio
 from services.ffmpeg_service import extract_audio
 
+from jobs.job_manager import (
+    create_job,
+    update_job,
+    finish_job,
+    get_job
+)
+
 app = FastAPI(
     title="LuminaDub Backend",
     version="1.0.0"
@@ -20,6 +27,14 @@ def home():
         "app": "LuminaDub AI"
     }
 
+@app.post("/job")
+def new_job():
+
+    job_id = create_job()
+
+    return {
+        "job_id": job_id
+    }
 
 @app.post("/detect-language")
 async def detect_language_audio(file: UploadFile = File(...)):
@@ -87,3 +102,7 @@ async def translate(
     return {
         "translated_text": translated
     }
+@app.get("/job/{job_id}")
+def job_status(job_id: str):
+
+    return get_job(job_id)
